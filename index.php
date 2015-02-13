7b2b904d42da4e02bab5b08bf8d7360927949d53
nemanjan00 <nemanjan00@gmail.com>
2015-02-13 12:34:11 +0100

    Final

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 		<link href="css/styles.css" rel="stylesheet">
 	</head>
 	<body>
-		<nav class="navbar navbar-fixed-top">
+		<nav class="navbar navbar-default navbar-fixed-top">
 			<div class="container">
 				<div class="navbar-header">
 				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
