2849c5ce16f6822b6209775350db130fae0f57d2
nemanjan00 <nemanjan00@gmail.com>
2015-02-24 22:09:19 +0100

    Test

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 		<!--[if lt IE 9]>
 			<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
-		<link href="css/styles.css" rel="stylesheet">
+		<link href="//cdn.rawgit.com/nemanjan00/sabacka-gimnazija/master/css/styles.css" rel="stylesheet">
 	</head>
 	<body>
 		<nav class="navbar navbar-default">
