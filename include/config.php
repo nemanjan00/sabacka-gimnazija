f5b55469d6e38b642bc06895ac4bdcff97210565
nemanjan00 <nemanjan00@gmail.com>
2015-02-25 21:26:29 +0100

    Test

--- include/config.php
+++ include/config.php
@@ -4,7 +4,7 @@ class config{
 
 	public function __construct(){
 		$this->config["pullzone"] = "https://gimnazija-113b.kxcdn.com/";
-		$this->config["pushzone"] = "https://gimnazija-113b.kxcdn.com/img/";
+		$this->config["pushzone"] = "https://cloudinary-113b.kxcdn.com/gimnazija/";
 	}
 
 	public function readValue($name){
