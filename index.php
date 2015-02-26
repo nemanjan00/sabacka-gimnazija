4767b6d62bbd1b11bd0c8b5b7e60b801236f3208
nemanjan00 <nemanjan00@gmail.com>
2015-02-26 14:40:23 +0100

    Test

--- index.php
+++ index.php
@@ -210,7 +210,7 @@ include("./include/resources.class.php");
 							<span aria-hidden="true">&laquo;</span>
 						</a>
 						</li>
-						<li><a href="#">1a</a></li>
+						<li><a href="#">1</a></li>
 						<li><a href="#">2</a></li>
 						<li><a href="#">3</a></li>
 						<li><a href="#">4</a></li>
