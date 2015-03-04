2298e04919c0fb6a5e3a71e18ee44b54c4808718
nemanjan00 <nemanjan00@gmail.com>
2015-03-04 08:03:57 +0100

    Page abstraction

--- include/config.php
+++ include/config.php
@@ -3,10 +3,11 @@ class config{
 	private $config = Array();
 
 	public function __construct(){
-		$this->config["pullzone"] = "https://sabackagimnazija-na1424817126.netdna-ssl.com/";
-		//$this->config["pullzone"] = "/";
+		//$this->config["pullzone"] = "https://sabackagimnazija-na1424817126.netdna-ssl.com/";
+		$this->config["pullzone"] = "/";
 
-		$this->config["pushzone"] = "https:////cloudinary-na1424817126.netdna-ssl.com/";
+		//$this->config["pushzone"] = "https:////cloudinary-na1424817126.netdna-ssl.com/";
+		$this->config["pushzone"] = "img/";
 	}
 
 	public function readValue($name){
--- include/page.class.php
+++ include/page.class.php
@@ -1,6 +1,14 @@
 <?php
-include("./include/config.php");
-include("./include/resources.class.php");
+class page{
+	private $content;
+
+	public function __construct($content){
+		$this->content = $content;
+	}
+
+	public function render(){
+		global $config;
+		global $resources;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -154,108 +162,7 @@ include("./include/resources.class.php");
 			<img src="<?=$resources->getStaticResource("img/logo.png");?>">
 		</header>
 
-		 <div class="sadrzaj container">
-			<div class="row">
-				<div id="myCarousel" class="col col-sm-12 carousel slide" data-ride="carousel">
-					<!-- Indicators -->
-					<ol class="carousel-indicators">
-						<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-						<li data-target="#myCarousel" data-slide-to="1"></li>
-						<li data-target="#myCarousel" data-slide-to="2"></li>
-					</ol>
-					<div class="carousel-inner" role="listbox">
-						<div class="item active">
-							<img src="<?=$resources->getDynamicResource("gimnazija/skola_01.jpg");?>" alt="First slide">
-							<div class="container">
-								<div class="carousel-caption">
-									<h1>Добродошли</h1>
-									<p>Контакт:<br>
-										sabackagimnazija@gmail.com<br>
-										sabackagimnazia@ptt.rs<br>
-										тел:015 350286<br>
-										Директор: 015 360570</p>
-									<p><a class="btn btn-lg btn-primary youtube" href="http://www.youtube.com/watch?v=drknOAAANwo" role="button">Погледајте видео... </a></p>
-								</div>
-							</div>
-						</div>
-						<div class="item">
-							<img src="<?=$resources->getDynamicResource("gimnazija/skola_02.jpg");?>" alt="Second slide">
-							<div class="container">
-								<div class="carousel-caption">
-									<h1>Another example headline.</h1>
-									<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-									<p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
-								</div>
-							</div>
-						</div>
-						<div class="item">
-							<img src="<?=$resources->getDynamicResource("gimnazija/skola_14.jpg");?>" alt="Third slide">
-							<div class="container">
-								<div class="carousel-caption">
-									<h1>One more for good measure.</h1>
-									<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-									<p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
-								</div>
-							</div>
-						</div>
-					</div>
-					<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-						<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-						<span class="sr-only">Previous</span>
-					</a>
-					<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-						<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-						<span class="sr-only">Next</span>
-					</a>
-				</div><!-- /.carousel -->
-
-				 <!--<div class="col col-sm-12">
-					<div class="panel">
-						<div class="panel-body">
-							 You may want to put some news here <span class="glyphicon glyphicon-heart-empty"></span>
-						</div>
-					</div>
-				</div>-->
-
-				<div class="col col-sm-12">
-					 <div class="panel">
-						<h1>Наслов</h1>
-						<p>Додато: 15.05.2014</p>
-
-						<img src="<?=$resources->getDynamicResource("gimnazija/skola_01.jpg");?>" class="glavna">
-
-						<h2>Поднаслов</h2>
-						Тотам рем апериам, еаqуе ипса qуае аб илло инвенторе веритатис ет qуаси арчитецто беатае витае дицта сунт еxплицабо. Немо еним ипсам волуптатем qуиа волуптас сит аспернатур аут одит аут фугит, сед qуиа цор магни долорес еос qуи ратионе волуптатем сеqуи несциунт. Неqуе порро qуисqуам ест, qуи долорем ипсум qуиа долор сит амет, цонсецтетур, адиписци велит, сед qуиа нон нумqуам еиус моди темпора инцидунт ут лаборе ет долоре магнам алиqуам qуаерат волуптатем. Ут еним ад минима вениам, qуис нострум еxерцитатионем уллам цорпорис сусципит лабориосам, ниси ут алиqуид еx еа цоммоди цонсеqуатур?
-						<br><br>
-
-						<button class="btn btn-lg btn-primary">Прочитајте више...</button>
-						<br><br>
-					 </div>
-				 </div>
-			 </div>
-
-			<nav>
-				<ul class="pagination">
-					<li>
-						<a href="#" aria-label="Previous">
-							<span aria-hidden="true">&laquo;</span>
-						</a>
-						</li>
-						<li><a href="#">1</a></li>
-						<li><a href="#">2</a></li>
-						<li><a href="#">3</a></li>
-						<li><a href="#">4</a></li>
-						<li><a href="#">5</a></li>
-						<li>
-						<a href="#" aria-label="Next">
-							<span aria-hidden="true">&raquo;</span>
-						</a>
-						</li>
-					</ul>
-				</nav>
-
-			<footer class="panel">&copy; Шабачка гимназија <?=date("Y");?> | CDN спонзорисао <a target="_blank" href="https://www.maxcdn.com/">MaxCDN</a> | Технички реализовао <a target="_blank" href="http://nemanjan00.com">Немања Недељковић</a></footer>
-		</div>
+		<?=$this->content;?>
 
 		<!-- script references -->
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
@@ -321,3 +228,6 @@ include("./include/resources.class.php");
 		</script>
 	</body>
 </html>
+<?php
+	}
+}
