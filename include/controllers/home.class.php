e9d284ec4f1ee043fa9e45e3c3ac93c022d1ce3d
nemanjan00 <nemanjan00@gmail.com>
2015-03-09 17:50:01 +0100

    Carousel

--- include/controllers/home.class.php
+++ include/controllers/home.class.php
@@ -62,60 +62,60 @@ class home extends controller{
 
 ?>
 
-		<div id="myCarousel" class="sadrzaj container">
-			<div class="col col-sm-12 carousel slide" data-ride="carousel">
-				<!-- Indicators -->
-				<ol class="carousel-indicators">
-					<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-					<li data-target="#myCarousel" data-slide-to="1"></li>
-					<li data-target="#myCarousel" data-slide-to="2"></li>
-				</ol>
-				<div class="carousel-inner" role="listbox">
-					<div class="item active">
-						<img src="<?=$this->getDynamicResource("gimnazija/skola_01.jpg");?>" alt="First slide">
-						<div class="container">
-							<div class="carousel-caption">
-								<h1>Добродошли</h1>
-								<p>Контакт:<br>
-									sabackagimnazija@gmail.com<br>
-									sabackagimnazia@ptt.rs<br>
-									тел:015 350286<br>
-									Директор: 015 360570</p>
-								<p><a class="btn btn-lg btn-primary youtube" href="http://www.youtube.com/watch?v=drknOAAANwo" role="button">Погледајте видео... </a></p>
-							</div>
-						</div>
-					</div>
-					<div class="item">
-						<img src="<?=$this->getDynamicResource("gimnazija/skola_02.jpg");?>" alt="Second slide">
-						<div class="container">
-							<div class="carousel-caption">
-								<h1>Another example headline.</h1>
-								<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-								<p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
-							</div>
-						</div>
-					</div>
-					<div class="item">
-						<img src="<?=$this->getDynamicResource("gimnazija/skola_14.jpg");?>" alt="Third slide">
-						<div class="container">
-							<div class="carousel-caption">
-								<h1>One more for good measure.</h1>
-								<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-								<p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
-							</div>
-						</div>
+<div class="sadrzaj container">
+	<div id="myCarousel" class="col col-sm-12 carousel slide" data-ride="carousel">
+		<!-- Indicators -->
+		<ol class="carousel-indicators">
+			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+			<li data-target="#myCarousel" data-slide-to="1"></li>
+			<li data-target="#myCarousel" data-slide-to="2"></li>
+		</ol>
+		<div class="carousel-inner" role="listbox">
+			<div class="item active">
+				<img src="<?=$this->getDynamicResource("gimnazija/skola_01.jpg");?>" alt="First slide">
+				<div class="container">
+					<div class="carousel-caption">
+						<h1>Добродошли</h1>
+						<p>Контакт:<br>
+							sabackagimnazija@gmail.com<br>
+							sabackagimnazia@ptt.rs<br>
+							тел:015 350286<br>
+							Директор: 015 360570</p>
+						<p><a class="btn btn-lg btn-primary youtube" href="http://www.youtube.com/watch?v=drknOAAANwo" role="button">Погледајте видео... </a></p>
 					</div>
 				</div>
-				<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-					<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-					<span class="sr-only">Previous</span>
-				</a>
-				<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-					<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-					<span class="sr-only">Next</span>
-				</a>
-			</div><!-- /.carousel -->
+			</div>
+			<div class="item">
+				<img src="<?=$this->getDynamicResource("gimnazija/skola_02.jpg");?>" alt="Second slide">
+				<div class="container">
+					<div class="carousel-caption">
+						<h1>Another example headline.</h1>
+						<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+						<p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+					</div>
+				</div>
+			</div>
+			<div class="item">
+				<img src="<?=$this->getDynamicResource("gimnazija/skola_14.jpg");?>" alt="Third slide">
+				<div class="container">
+					<div class="carousel-caption">
+						<h1>One more for good measure.</h1>
+						<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+						<p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+					</div>
+				</div>
+			</div>
 		</div>
+		<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
+	</div><!-- /.carousel -->
+</div>
 
 <?php
 
